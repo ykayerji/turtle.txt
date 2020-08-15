60fad38d1352c5f6c792b13f6ba1b4adcdf1dfb4
ykayerji <55718660+ykayerji@users.noreply.github.com>
2020-08-15 03:24:36 -0400

    Add files via upload

---
+++ turtles.py
@@ -0,0 +1,11 @@
+import turtle
+yk = turtle.Turtle()
+yk.speed(0)
+for i in range(766):
+ yk.forward(787)
+ yk.right(138)
+for i in range(76678):
+ yk.forward(644)
+ yk.right(455)
+input('hit enter to exit')
+
